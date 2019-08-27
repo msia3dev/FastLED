a6057f86d24dfb417577a5fb6c5765749c5a2fd3
Mladen Đurić <msia.dev@gmail.com>
2019-08-27 02:27:20 +0200

    feat(stm32): add dma support

--- platforms/arm/stm32/fastled_arm_stm32.h
+++ platforms/arm/stm32/fastled_arm_stm32.h
@@ -3,7 +3,10 @@
 
 // Include the sam headers
 #include "fastpin_arm_stm32.h"
-// #include "fastspi_arm_stm32.h"
-#include "clockless_arm_stm32.h"
+#ifdef FASTLED_STM32_DMA
+    #include "clockless_spi_stm32.h"
+#else
+    #include "clockless_arm_stm32.h"
+#endif
 
 #endif
